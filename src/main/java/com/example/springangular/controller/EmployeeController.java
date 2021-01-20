ab0e6378871d9be629ed3b836be73057493cbfb1
heshan_c <heshanamila94@gmail.com>
2021-01-20 16:06:17 +0530

    commit 1

--- src/main/java/com/example/springangular/controller/EmployeeController.java
+++ src/main/java/com/example/springangular/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.example.springangular.controller;
 import com.example.springangular.entity.Employee;
 import com.example.springangular.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +16,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
+    }
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 
 }
